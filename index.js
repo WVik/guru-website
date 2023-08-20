2cf3c7e58a03f4576ea0932a9356464b26c219a7
“Vikram <wvikram11@gmail.com>
2023-08-19 18:29:27 -0700

    add downloads folder and create downloads page

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var currentImageIndex = 0;
-var images = document.querySelectorAll('.carousel-image');
+// var currentImageIndex = 0;
+// var images = document.querySelectorAll('.carousel-image');
 
-function changeImage(step) {
-  images[currentImageIndex].classList.remove('active');
-  currentImageIndex = (currentImageIndex + step + images.length) % images.length;
-  images[currentImageIndex].classList.add('active');
-}
+// function changeImage(step) {
+//   images[currentImageIndex].classList.remove('active');
+//   currentImageIndex = (currentImageIndex + step + images.length) % images.length;
+//   images[currentImageIndex].classList.add('active');
+// }
 
-// Show the first image initially
-images[0].classList.add('active');
+// // Show the first image initially
+// images[0].classList.add('active');
 
 function openPopup(element) {
     var content = element.innerHTML;
@@ -19,3 +19,23 @@ function openPopup(element) {
 function closePopup() {
     document.getElementById("popup").style.display = "none";
 }
+
+document.getElementById('openModal').addEventListener('click', function() {
+  fetch('contactModal.html')
+  .then(response => response.text())
+  .then(content => {
+      // Add modal content to the body
+      document.body.innerHTML += content;
+
+      // Use Bootstrap's modal function to display it
+      $('#contactModal').modal('show');
+
+      // Add an event listener to remove the modal from the DOM after it's hidden
+      $('#contactModal').on('hidden.bs.modal', function (e) {
+          document.getElementById('contactModal').remove();
+      });
+  })
+  .catch(error => {
+      console.error('Error fetching the modal content:', error);
+  });
+});
